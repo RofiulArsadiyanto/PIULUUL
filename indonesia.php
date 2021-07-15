5179250a28fd9974e4e903e9850eaba86f01be05
RofiulArsadiyanto <74356824+RofiulArsadiyanto@users.noreply.github.com>
2021-07-15 15:50:46 +0700

    Add files via upload

---
+++ indonesia.php
@@ -0,0 +1,38 @@
+<?php
+
+//menu utama
+define ("language", 'Bahasa');
+define ("home", 'Beranda');
+define ("services", 'Program Pendidikan');
+define ("gallery", 'Galleri');
+define ("aboutus", 'Tentang Kami');
+define ("contactus", 'Kontak');
+
+//tombol
+define ("moredetail", "Lihat Program Pendidikan Kami");
+define ("buttoncontinue", "lihat Galeri Kami");
+define ("buttonsendemail", "Kirim Pesan");
+
+//judul
+define ("labelgallery", "Galeri");
+define ("labelgallerydescription", "Galeri Foto Kegiatan Belajar ");
+define ("labelservices", "Program Pendidikan");
+define ("labelaboutus", "Sekilas Tentang Kami");
+define ("labelcontactus", "Kontak Kami");
+
+//form email
+define ("placename", "Nama Anda...");
+define ("placesubject", "Subjek...");
+define ("placeemail", "Alamat Email Anda...");
+define ("placemessage", "Isi Pesan...");
+define ("buttonsendmail", "Kirim Pesan");
+
+//footer
+define ("labellocation", "Lokasi");
+define ("labelinfo", "Informasi Lengkap");
+define ("labelheadoffice", "Kantor Pusat");
+define ("labelphone", "Telepon");
+define ("labelemail", "Email");
+define ("labelsosmed", "Media Sosial");
+
+?>
